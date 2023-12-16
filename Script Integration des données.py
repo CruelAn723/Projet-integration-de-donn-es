390533239afb7f1a7ac4baeb3838db591280b6b5
CruelAn723 <153769832+CruelAn723@users.noreply.github.com>
2023-12-16 12:03:55 +0100

    Update Script Integration des données.py
    correction des erreurs de mise à jour de base de donnée
    - la base de donnée n'était mise à jour qu'à la fin et coups il n'a y que le dernier batch qui était inséré dans la base

--- Script Integration des données.py
+++ Script Integration des données.py
@@ -11,9 +11,9 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-batch_size = 50  # Number of code_INSEEs to fetch in each batch
+batch_size = 95  # Number of code_INSEEs to fetch in each batch
 
-# On selecctionne les lignes dont la population est nulle 
+# On selecctione les lignes dont la population est nulle 
 cursor.execute("SELECT Code_commune_INSEE FROM communes WHERE Population IS NULL")
 rows = cursor.fetchall()
 
@@ -25,7 +25,7 @@ endURL = "?fields=population&format=json"
 api_responses = []
 population_data = []
 
-# on divise les lignes en batch et On construits les requetes vers l'api
+# on divise les lignes en batch et On construit les requetes vers l'api
 for i in range(0, len(rows), batch_size):
     current_batch = rows[i:i + batch_size]
 
@@ -35,15 +35,15 @@ for i in range(0, len(rows), batch_size):
     print(f"\nProcessing batch {i//batch_size + 1}...")
     print("API URLs:", api_urls)
 
-    # On envoie des requettes d'Api en parralle grace à ThreadPoolExecutor
+    # On envoie des requetes d'Api en parallele grace à ThreadPoolExecutor
     try:
         with ThreadPoolExecutor() as executor:
             responses = list(executor.map(requests.get, api_urls))
     except Exception as e:
         print(f"Error in API request: {e}")
-        # on Log les erreurs et les  exceptions
+        # on Log les erreurs et les exceptions
 
-    # On extrait les données de population de la reponse de l'API
+    # On extrait les données de population de la réponse de l'API
     for j, response in enumerate(responses):
         api_responses.append(response)
 
@@ -56,21 +56,17 @@ for i in range(0, len(rows), batch_size):
             print(f"Error parsing response: {e}")
             # on Log les données et les erreurs si necessaire 
 
-# On mets a jour la base de données 
-for i, row in enumerate(current_batch):
-    code_INSEE = str(row[0]).zfill(5)
-    population = population_data[i]
+        # On met à jour la base de données pour chaque ligne
+        code_INSEE = str(current_batch[j][0]).zfill(5)
+        try:
+            cursor.execute("UPDATE communes SET Population = %s WHERE Code_commune_INSEE = %s", (population, code_INSEE))
+            print(f"Updated population for {code_INSEE}: {population}")
+        except Exception as e:
+            print(f"Error updating database: {e}")
+            # on Log les données et les erreurs si necessaire 
 
-    try:
-        cursor.execute("UPDATE communes SET Population = %s WHERE Code_commune_INSEE = %s", (population, code_INSEE))
-        print(f"Updated population for {code_INSEE}: {population}")
-    except Exception as e:
-        print(f"Error updating database: {e}")
-        # on Log les données et les erreurs si necessaire 
-
-
-# on Commit les modifications
+# On Commit les modifications
 conn.commit()
 
-# on ferme la connection
+# On ferme la connexion
 conn.close()
